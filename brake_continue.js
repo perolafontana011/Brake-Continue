293c93a57e179de758a08373e6d0c9f5aaa1ac89
Pérola <perola.corchog@escola.pr.gov.br>
2026-03-12 09:14:05 -0300

    Update brake_continue.js

--- brake_continue.js
+++ brake_continue.js
@@ -1,8 +1,8 @@
 let carros_estacionados = 0;
-const limitemaximo = 4;
+const limitemaximo = 7;
 console.log("---Iniciando verificação de vagas---")
 for (let vaga = 1; vaga <= 10; vaga ++ ){
-    if(vaga === 4 || vaga === 7){
+    if(vaga === 4 || vaga === 7 || vaga === 3){
         console.log(`Vaga ${vaga}: [Interditada]-Pulando...`);
         continue;
     }
@@ -13,4 +13,5 @@ for (let vaga = 1; vaga <= 10; vaga ++ ){
         break;
     }
 }
+
 console.log("Relatório final: Operação de entrada encerrada");
